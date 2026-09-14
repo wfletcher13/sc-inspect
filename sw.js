8ceac0c03c52fe42ec260505177bbe1eca55fbd6
wfletcher13 <wfletcher13@gmail.com>
2026-09-14 17:31:13 -0400

    Scanning a setup code saves, connects and syncs without a manual Save: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SC Inspect service worker: caches the app shell so it opens without a connection.
-const VERSION = 'sc-inspect-ffcc9e3c';
+const VERSION = 'sc-inspect-90ea3125';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(VERSION).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
